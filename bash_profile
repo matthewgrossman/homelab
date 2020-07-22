99f8e616809dd3fd4141c58cc092442211289dbd
Matthew Grossman <matthewryangrossman@gmail.com>
2020-07-21 22:26:57 -0700

    add in generic bash_profile

---
+++ bash_profile
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+export GITHUB_SSH_URL="git@github.com:matthewgrossman/lab.git"
--- vps/setup.sh
+++ vps/setup.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
+
+# setup bash_profile
+source "$HOME/lab/vps/bash_profile"
 ln -s "$HOME/lab/vps/bash_profile" "$HOME/.bash_profile"
+
+# unencrypt ssh keys from pass
+pass vps/id_rsa > "$HOME/.ssh/id_rsa"
+pass vps/id_rsa.pub > "$HOME/.ssh/id_rsa.pub"
+
+# now that we have our ssh keys, modify lab repo to use ssh instead of https
+git -C "$HOME/lab" remote set-url "$GITHUB_SSH_URL"
